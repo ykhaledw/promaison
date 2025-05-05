c0a91caf6870fa6343e6cd396bec0e9cf5739abe
youssefkhaled <youssefwahdan12@gmail.com>
2025-05-05 23:29:54 +0300

    - added pretty_dio_logger package - created dio_factory file to manage dio instances - created api_service file to add api endpoints

---
+++ lib/core/networking/api_service.dart
@@ -0,0 +1,9 @@
+import 'package:dio/dio.dart';
+import 'package:promaison/core/networking/api_constants.dart';
+import 'package:retrofit/retrofit.dart';
+part 'api_service.g.dart';
+
+@RestApi(baseUrl: ApiConstants.baseUrl)
+abstract class ApiService {
+  factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+}
