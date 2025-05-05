8f0c63e22d6d1e583242d669167ee8e14176874c
youssefkhaled <youssefwahdan12@gmail.com>
2025-05-05 23:54:52 +0300

    - created file for login inputs 'login_request_body' - created file for login response 'login_response_body' - added login method in api_service file

--- lib/core/networking/api_service.dart
+++ lib/core/networking/api_service.dart
@@ -1,9 +1,17 @@
 import 'package:dio/dio.dart';
 import 'package:promaison/core/networking/api_constants.dart';
+import 'package:promaison/features/login/data/models/login_response_body.dart';
 import 'package:retrofit/retrofit.dart';
+
+import '../../features/login/data/models/login_request_body.dart';
 part 'api_service.g.dart';
 
 @RestApi(baseUrl: ApiConstants.baseUrl)
 abstract class ApiService {
   factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+
+  @POST(ApiConstants.loginUrl)
+  Future<LoginResponseBody> loginByPhoneNumber(
+    @Body() LoginRequestBody loginRequestBody,
+  );
 }
---
+++ lib/features/login/data/models/login_request_body.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login_request_body.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+LoginRequestBody _$LoginRequestBodyFromJson(Map<String, dynamic> json) =>
+    LoginRequestBody(
+      phoneNumber: json['phoneNumber'] as String,
+    );
+
+Map<String, dynamic> _$LoginRequestBodyToJson(LoginRequestBody instance) =>
+    <String, dynamic>{
+      'phoneNumber': instance.phoneNumber,
+    };
